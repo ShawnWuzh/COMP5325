3768edc4432e62691209852335b890f062ec837c
Rex Wong <freemail0062@gmail.com>
2017-11-21 20:56:24 +0800

    test run

--- GlobVar.py
+++ GlobVar.py
@@ -1,15 +1,7 @@
-"""
-  SERVER_STATUS:
-  UNKNOWN - Wait client interface to provide initial value
-  STANDBY - Ony to receive synchronize request and ready to pick up primary role
-  PRIMARY - Primary server wo recvie requests from client
- """
-
 class Globvar:
   _ONE_DAY_IN_SECONDS = 60 * 60 * 24
   SYNC_PORT = 50002
   ACTION_ID = 0
-  SERVER_STATUS = "UNKNOWN"
   # in terms of seconds
   CHECKPOINT_DURATION  = 15
   # in terms of seconds
