f4708bfadab9abb53f5ac35d69c175113fdc4f87
Zac Wu <wuzh@ZAC-MacBook-Pro.local>
2017-12-03 15:54:42 +0800

    client version 1

--- Client.py
+++ Client.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """The Python implementation of the GRPC helloworld.Greeter client."""
 
 from __future__ import print_function
+import json
+
 
 import grpc
 import sys
@@ -48,32 +49,55 @@ def run():
     try:
       channel = grpc.insecure_channel('{}:50051'.format(addr_list[primary]))
       stub = ClientRequest_pb2_grpc.GreeterStub(channel)
-      print("---------------------------------------")
-      print("* Enter you operation:                *")
-      print("*    1: Look up balance               *")
-      print("*    2: Withdraw an amount of money   *")
-      print("*    3: Save an amount of money       *")
-      print("*    4: Terminating program           *")
-      print("---------------------------------------\n")
-      op = input("Your choice: ")
-      if op == "1":
-        lookup_balance(stub)
-      elif op == "2":
-        withdrawal(stub)
-      elif op == "3":
-        deposit(stub)
-      else:
-        sys.exit()
-      time.sleep(3)
-      # for windows :
-      os.system('cls')
-      # for linux:
-      os.system('clear')
     except:
       if primary == 's1':
         primary = 's2'
       else:
         primary = 's1'
+    print()
+    print('Welcome to Distributed Bank!')
+    print("------------------------------------------------------")
+    print("* Enter you operation:                *")
+    print("*    1: Look up balance               *")
+    print("*    2: Withdraw an amount of money   *")
+    print("*    3: Save an amount of money       *")
+    print("*    4: Terminating program           *")
+    print("------------------------------------------------------\n")
+    op = input("Your choice: ")
+    if op == "1":
+      try:
+        lookup_balance(stub)
+      except:
+        if primary == 's1':
+          primary = 's2'
+        else:
+          primary = 's1'
+        lookup_balance(stub)
+    elif op == "2":
+      try:
+        withdrawal(stub)
+      except:
+        if primary == 's1':
+          primary = 's2'
+        else:
+          primary = 's1'
+          withdrawal(stub)
+    elif op == "3":
+      try:
+        deposit(stub)
+      except:
+        if primary == 's1':
+          primary = 's2'
+        else:
+          primary = 's1'
+        deposit(stub)
+    else:
+      sys.exit()
+    time.sleep(3)
+    # for windows :
+    os.system('cls')
+    # for linux:
+    os.system('clear')
 
     #response = stub.GetResult(ClientRequest_pb2.ClientRequest(actionType=username))
     #print("Greeter client received: " + str(response.responseAmt))
